b5f562e0a8683a293117b7016b6784bcf8a363eb
beaseobar <beaseobar@gmail.com>
2025-07-23 19:18:03 +0200

    solved bug training rdm

--- rbms/training/pcd.py
+++ rbms/training/pcd.py
@@ -124,10 +124,13 @@ def train(
             rand_idx = torch.randperm(len(train_dataset))[: args["batch_size"]]
             batch = (train_dataset.data[rand_idx], train_dataset.weights[rand_idx])
             if args["training_type"] == "rdm":
-                parallel_chains = params.init_chains(parallel_chains["v"].shape[0])
+                
+                parallel_chains = params.init_chains(parallel_chains["visible"].shape[0])
             elif args["training_type"] == "cd":
                 parallel_chains = params.init_chains(batch[0].shape[0],weights=batch[1], start_v=batch[0])
             optimizer.zero_grad(set_to_none=False)
+
+            
             parallel_chains, logs = fit_batch_pcd(
                 batch=batch,
                 parallel_chains=parallel_chains,
