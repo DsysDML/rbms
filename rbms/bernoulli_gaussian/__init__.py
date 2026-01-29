9da903c546b677036e30979646313b9d2d17816b
Nicolas Béreux <nicolas.bereux@gmail.com>
2026-01-29 13:09:43 +0100

    fix: add __init__ to bernoulli_gaussian

---
+++ rbms/bernoulli_gaussian/__init__.py
@@ -0,0 +1,12 @@
+# ruff: noqa
+from rbms.bernoulli_gaussian.classes import BGRBM
+from rbms.bernoulli_gaussian.functional import (
+    compute_energy,
+    compute_energy_hiddens,
+    compute_energy_visibles,
+    compute_gradient,
+    init_chains,
+    init_parameters,
+    sample_hiddens,
+    sample_visibles,
+)
