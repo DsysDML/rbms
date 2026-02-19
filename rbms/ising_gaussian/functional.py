76db82bf7820eff1bb4da2297dc451218a589d9a
Francill66 <129966239+Francill66@users.noreply.github.com>
2026-02-19 11:02:44 +0100

    Update functional.py

--- rbms/ising_gaussian/functional.py
+++ rbms/ising_gaussian/functional.py
@@ -83,7 +83,7 @@ def compute_gradient(
         mh_data=data["hidden_mag"],
         w_data=data["weights"],
         v_chain=chains["visible"],
-        h_chain=chains["hidden"],
+        h_chain=chains["hidden_mag"],
         w_chain=chains["weights"],
         vbias=params.vbias,
         hbias=params.hbias,
