74361bcd85d807d1ddfbcc249defa36d7c4b415f
Aidan Liotard <aidan.liotard@gmail.com>
2026-04-23 16:32:54 +0200

    update var init

--- rbms/bernoulli_bernoulli/implement.py
+++ rbms/bernoulli_bernoulli/implement.py
@@ -150,8 +150,9 @@ def _init_parameters(
     data: Tensor,
     device: torch.device,
     dtype: torch.dtype,
-    var_init: float = 1e-4,
+    var_init: float = 1e-2,
 ) -> tuple[Tensor, Tensor, Tensor]:
+    var_init: float = 1e-2
     _, num_visibles = data.shape
     eps = 1e-4
     weight_matrix = (
