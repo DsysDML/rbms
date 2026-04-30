d1dde0df4986d4a0605c6b46f3d62509996466c8
Aidan Liotard <aidan.liotard@gmail.com>
2026-04-30 12:25:07 +0200

    update_h_gradient

--- rbms/EBM_binary/energies.py
+++ rbms/EBM_binary/energies.py
@@ -30,7 +30,7 @@ class MLPEnergy(torch.nn.Module):
 
         if visible_field is None:
             visible_field = torch.zeros(num_visibles)
-        self.register_buffer("visible_field", visible_field.clone())
+        self.visible_field = torch.nn.Parameter(visible_field.clone())
 
         layers = []
         in_dim = num_visibles
